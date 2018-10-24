2eb5f45f4e3958a7c2a2020407d901fad6e87cc9
Lars Berg Hustveit <lars.berg@hustve.it>
2018-10-24 14:40:27 +0200

    Removing test data.

--- PersonArchive/PersonArchive.Web/DataContexts/DbInitializer.cs
+++ PersonArchive/PersonArchive.Web/DataContexts/DbInitializer.cs
@@ -1,74 +1,9 @@
-﻿//using System;
-//using System.Linq;
-//using PersonArchive.Entities.PersonDbContext;
-
-namespace PersonArchive.Web.DataContexts
+﻿namespace PersonArchive.Web.DataContexts
 {
 	public static class DbInitializer
 	{
 		public static void Seed(PersonDbContext context)
 		{
-			/*
-			if (context.Persons.Any())
-				return; // Db has been seeded
-
-			var persons = new[]
-			{
-				new Person
-				{
-					Gender = PersonGender.Male,
-					Names = new[]
-					{
-						new PersonName
-						{
-							First = "Lars",
-							NameWeight = 1,
-							TimeCreated = DateTime.Now,
-							TimeLastUpdated = DateTime.Now
-						}
-					},
-					Descriptions = new[]
-					{
-						new PersonDescription
-						{
-							Description = "Lars kjem frå Sogndal.",
-							Type = PersonDescriptionType.Norwegian,
-							TimeCreated = DateTime.Now,
-							TimeLastUpdated = DateTime.Now
-						}
-					},
-					FluffyDates = new[]
-					{
-						new PersonFluffyDate
-						{
-							Year = 1985,
-							Type = PersonFluffyDateType.Birth,
-							TimeCreated = DateTime.Now,
-							TimeLastUpdated = DateTime.Now
-						}
-					},
-					TimeCreated = DateTime.Now,
-					TimeLastUpdated = DateTime.Now
-				},
-				new Person
-				{
-					Gender = PersonGender.Female,
-					TimeCreated = DateTime.Now,
-					TimeLastUpdated = DateTime.Now
-				},
-				new Person
-				{
-					Gender = PersonGender.Undefined,
-					TimeCreated = DateTime.Now,
-					TimeLastUpdated = DateTime.Now
-				}
-			};
-
-			foreach (var person in persons)
-				context.Persons.Add(person);
-
-			context.SaveChanges();
-			*/
 		}
 	}
 }
